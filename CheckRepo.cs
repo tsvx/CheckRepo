569478eddafa3cd87d1283491c4bdac925887829
Tema <tsul@mail.ru>
2016-03-17 07:12:45 +0300

    Remove unreferenced files (flag `-r`).
    Portability improvements for Mono/Linux (not tested).

--- CheckRepo.cs
+++ CheckRepo.cs
@@ -45,9 +45,9 @@ namespace CheckRepo
 
 		static bool UpdateFile(string baseUrl, string dir, string fname)
 		{
-			string fullDir = Path.GetDirectoryName( Path.Combine(dir,fname));
-			if (!Directory.Exists(fullDir))
-				Directory.CreateDirectory(fullDir);
+			string dirName = Path.GetDirectoryName(dir + fname);
+			if (!Directory.Exists(dirName))
+				Directory.CreateDirectory(dirName);
 			bool? r = HttpUtils.DownloadFile(baseUrl + fname, dir + fname, DownloadProgress);
 			Console.WriteLine(r == true ? "OK" : r == null ? "Error" : "Fatal");
 			return true == r;
@@ -55,7 +55,7 @@ namespace CheckRepo
 
 		static CheckResult CheckFile(string dir, RepoFileInfo f, ref string msg, bool checkHash)
 		{
-			string fname = Path.Combine(dir, f.Name);
+			string fname = dir + f.Name;
 			var fi = new FileInfo(fname);
 			if (!fi.Exists)
 			{
@@ -103,7 +103,7 @@ namespace CheckRepo
 				return false;
 			Console.WriteLine("Downloading file '{0}' ...", f.Name);
 			if (r != CheckResult.NotExist)
-				File.Delete(Path.Combine(dir, f.Name));
+				File.Delete(dir + f.Name);
 			if (!UpdateFile(url, dir, f.Name))
 			{
 				Console.WriteLine("Error");
@@ -127,22 +127,22 @@ namespace CheckRepo
 				Console.WriteLine("Fatal: dir '{0}' does not exist.", dir);
 				return false;
 			}
-			dir = dir.TrimEnd(Path.DirectorySeparatorChar) + Path.DirectorySeparatorChar;
+			string urlFile = dir + ".url";
 			if (url == "")
-				url = File.ReadLines(dir + ".url").First();
+				url = File.ReadLines(urlFile).First();
 			else if (url != null)
 			{
 				url = url.TrimEnd('/') + '/';
-				File.WriteAllText(dir + ".url", url);
+				File.WriteAllText(urlFile, url);
 			}
 			if (url != null)
 				Console.WriteLine("Update from " + url);
+			string repomd = dir + MainRepoFile;
 			if (list != null)
 			{
-				list.Add(".url");
-				list.Add(MainRepoFile);
+				list.Add(urlFile);
+				list.Add(repomd);
 			}
-			string repomd = Path.Combine(dir, MainRepoFile);
 			if (!File.Exists(repomd) && url == null)
 			{
 				Console.WriteLine("Fatal: Main file '{0}' does not exist.", MainRepoFile);
@@ -156,7 +156,7 @@ namespace CheckRepo
 					File.Delete(bak);
 					File.Move(repomd, bak);
 					if (list != null)
-						list.Add(MainRepoFile + ".bak");
+						list.Add(bak);
 				}
 				UpdateFile(url, dir, MainRepoFile);
 			}
@@ -175,7 +175,7 @@ namespace CheckRepo
 			foreach (var f in ff)
 			{
 				if (list != null)
-					list.Add(f.Name);
+					list.Add(dir + f.Name);
 				if (!CheckAndUpdate(url, dir, f, checkHash))
 				{
 					badFiles++;
@@ -205,7 +205,7 @@ namespace CheckRepo
 		{
 			int badFiles = 0;
 			Console.WriteLine("Checking rpm files...");
-			prname = Path.Combine(dir, prname);
+			prname = dir + prname;
 			Stream prfs = File.OpenRead(prname);
 			if (prname.EndsWith(".gz"))
 				prfs = new GZipStream(prfs, CompressionMode.Decompress);
@@ -223,7 +223,7 @@ namespace CheckRepo
 			foreach (var f in ff)
 			{
 				if (list != null)
-					list.Add(f.Name);
+					list.Add(dir + f.Name);
 				if (!CheckAndUpdate(url, dir, f, checkHash))
 					badFiles++;
 				else if (f.Type != "rpm")
@@ -236,32 +236,69 @@ namespace CheckRepo
 			return badFiles;
 		}
 
+		static void ShowExcess(string dir, SortedSet<string> keepFiles, bool deleteExcess)
+		{
+			var allFiles = Directory.EnumerateFiles(dir, "*", SearchOption.AllDirectories);
+			if (Path.AltDirectorySeparatorChar != Path.DirectorySeparatorChar)
+				allFiles = allFiles.Select(s => s.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar));
+			var toDel = new SortedSet<string>(allFiles);
+			toDel.ExceptWith(keepFiles);
+			if (toDel.Count > 0)
+				Console.WriteLine("Excess files:");
+			foreach (var f in toDel)
+				if (deleteExcess)
+				{
+					Console.Write(f);
+					File.Delete(f);
+					Console.WriteLine(" removed.");
+				}
+				else
+					Console.WriteLine(f);
+			// TODO: Remove empty dirs.
+			if (toDel.Count > 0 && deleteExcess)
+				Console.WriteLine("Done.");
+		}
+		
 		static int Main(string[] args)
 		{
 			string repoDir = null;
 			string updateUrl = null;
-			SortedSet<string> filesList = null; 
+			SortedSet<string> filesList = new SortedSet<string>();
+			bool deleteExcess = false;
 			bool checkHash = false;
 
 			foreach (var s in args)
 			{
 				if (s == "-r")
-					filesList = new SortedSet<string>();
-				if (s == "-c")
+					deleteExcess = true;
+				else if (s == "-c")
 					checkHash = true;
 				else if (s == "-u")
 					updateUrl = "";
 				else if (updateUrl == "")
 					updateUrl = s;
-				else
+				else if (Directory.Exists(s))
 					repoDir = s;
+				else
+				{
+					Console.WriteLine("Fatal: unknown argument: " + s);
+					return 1;
+				}
 			}
 			if (repoDir == null)
 				repoDir = ".";
 
+			if (Path.DirectorySeparatorChar != Path.AltDirectorySeparatorChar)
+				repoDir = repoDir.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+			repoDir = repoDir.TrimEnd('/') + '/';
+
 			bool r = CheckRepo(repoDir, updateUrl, filesList, checkHash);
 			if (r)
+			{
 				Console.WriteLine("The repo is OK.");
+				if (filesList != null)
+					ShowExcess(repoDir, filesList, deleteExcess);
+			}
 			else
 				Console.WriteLine("Bad repo.");
 			return r ? 0 : 1;
--- README.md
+++ README.md
@@ -2,14 +2,25 @@
 Verification and update of local RPM repos.
 *For those who have to update offline linux servers:)*
 
+## Annotation
+
+I serve some offline linux servers and want to keep them up to date. For this reason I use rsync to get fresh repositories.
+Sometimes rsync gives bad results. Even if it seems that everithing is OK. So I need a tool to check an offline mirror of a repo.
+
+Besides, some small repos do not support rsync (mono-opt repos, for instance), only http.
+So I need a tool to sync them over http.
+
+Here such a tool.
+
 ## Usage:
 
-`CheckRepo [dir] [-c] [-u [url]]`
+`CheckRepo [dir] [-c] [-r] [-u [url]]`
 
 1. Checks a repo in the given or in the current directory;
 2. Checksum verification is performed only if the flag `-c` is specified.
-3. Update files, if flag `-u` is given. Update url, if given, is written to the file `.url`
+3. Update files, if the flag `-u` is given. Update url, if given, is written to the file `.url`
 in the dir to be checked. Or is read from this file if it is not given.
+4. Remove redundant files, if the flag `-r` is given and the repo is OK.
 
 The update process tries to download `repomd.xml` (always) and each file referenced
 that is absent or having a wrong size or checksum (if the latter is supposed to verify).
@@ -28,4 +39,4 @@ After that, it outputs whenever the [updated] repository is consistent.
 If something goes wrong, the app outputs it.
 
 ### To do
-Flag `-r` to remove unwanted files which are no longer referred to this repo.
+Remove empty dirs in remove mode (`-r`).
